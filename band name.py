6a019ace419fcdcaa85cc795af16b5e8e032bb98
kevin Samuel <kevinofficial843@gmail.com>
2024-04-04 23:09:02 +0530

    First Commit
    Apr 4 2024

---
+++ band name.py
@@ -0,0 +1,5 @@
+print("Super Simple Band Name Generator")
+pet= input("Please enter the name of your pet: ")
+place= input("Please enter the name of the country you were born in: ")
+band= pet + " " + place
+print("that's amazing, your band name is " +band)
